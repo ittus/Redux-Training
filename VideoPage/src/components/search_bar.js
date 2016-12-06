0fbb08efe38a901e92051d026aaf6d5c6ff911c5
ittus <vuminhthang.cm@gmail.com>
2016-12-07 01:06:27 +0800

    VideoPage - Basic structure

---
+++ VideoPage/src/components/search_bar.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+
+// const SearchBar = () => {
+//     return <input></input>;
+// }
+
+class SearchBar extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            term: ''
+        };
+    }
+
+    render() {
+        return (
+            <div>
+                <input
+                    value={this.state.term} 
+                    onChange={this.onInputChange.bind(this)} />
+            </div>
+        );
+    }
+
+    onInputChange(event) {
+        this.setState({
+            term: event.target.value
+        });
+    }
+
+}
+
+export default SearchBar;
