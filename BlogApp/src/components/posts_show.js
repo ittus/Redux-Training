a1bf4ac82eb83cbef6319a677b3bb05f2eb115dd
ittus <vuminhthang.cm@gmail.com>
2016-12-10 14:22:02 +0800

    Blog App - Create post details page

---
+++ BlogApp/src/components/posts_show.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchPost } from '../actions/index';
+import { Link } from 'react-router';
+
+class PostsShow extends Component {
+    componentWillMount() {
+        this.props.fetchPost(this.props.params.id);
+    }
+
+    render() {
+        if (!this.props.post) {
+            return <div>Loading...</div>;
+        }
+        const { post } = this.props;
+        return (
+            <div>
+                <Link to='/'>Back to Index</Link>
+                <h3>{post.title}</h3>
+                <h6>{post.categories}</h6>
+                <p>{post.content}</p>
+            </div>
+        );
+    }
+}
+
+function mapStateToProps(state) {
+    return {
+        post: state.posts.post
+    }
+}
+export default connect(mapStateToProps, { fetchPost })(PostsShow);
